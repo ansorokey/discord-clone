280d157d0a94ac0ab76ce0f7939ab24edc9204fb
ansorokey <ansorokey@gmail.com>
2024-01-04 20:31:56 -0500

    modal styled

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -119,7 +119,7 @@ export function CreateServerModal() {
                                             <Input
                                                 disabled={isLoading}
                                                 className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
-                                                placeholder='Enter Sever Name'
+                                                placeholder='Enter Server Name'
                                                 {...field}
                                             />
                                         </FormControl>
