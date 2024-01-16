594cfcb899de57d8663b9fc12f2c48e1501bf1f0
ansorokey <ansorokey@gmail.com>
2024-01-16 15:09:58 -0500

    created socket provider

--- dev-log.md
+++ dev-log.md
@@ -1,18 +1,2 @@
-Creating the channel id page
-Start time:
-[https://youtu.be/ZbX4Ok9YX94?t=25938]
-
-resume
-[https://youtu.be/ZbX4Ok9YX94?t=26204]
-
-
-
-problem with getting ht page to load
-take a look at our redirect routes
-page loads when I remove the sign-in=url portion
-[https://youtu.be/ZbX4Ok9YX94?t=26677]
-Or not? Works perfectly fine on home wifi now.
-Whatever.
-
-when we click on a user, we first need to check if we have a dm open
-otherwise, we need to create one
+socketIo setup
+[https://youtu.be/ZbX4Ok9YX94?t=29320]
--- types.ts
+++ types.ts
@@ -1,7 +1,17 @@
-// declare custom data types
-
+import { Server as NetServer, Socket} from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
 import { Server, Member, Profile } from "@prisma/client"
 
+// create custom types here
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile })[]
+}
+
+export type NextApiResponseServerIo = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io: SocketIOServer;
+        }
+    }
 }
